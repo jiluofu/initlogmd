3a860926b88496a9660d327f5e423c602f070fa7
zhuxu <zhuxu@baidu.com>
2018-04-08 11:01:57 +0800

    =update

--- lib/init.py
+++ lib/init.py
@@ -9,35 +9,35 @@ from string import Template
 startNum = 760
 startNumDate = '2018-03-08'
 
-d1Num = 91
-d1Date = '2018-03-19'
+d1Num = 94
+d1Date = '2018-04-09'
 d1Cate = '看图说话'
 
-d2Num = 382
-d2Date = '2018-03-20'
+d2Num = 392
+d2Date = '2018-04-10'
 d2Cate = '与喵共舞'
 
-d3Num = 58
-d3Date = '2018-03-21'
+d3Num = 61
+d3Date = '2018-04-11'
 d3Cate = '读书'
 
-d4Num = 84
-d4Date = '2018-03-22'
+d4Num = 86
+d4Date = '2018-04-19'
 d4Cate = '朝花夕拾'
-d4Num1 = 36
-d4Date1 = '2018-03-15'
+d4Num1 = 38
+d4Date1 = '2018-04-12'
 d4Cate1 = '读历史'
 
-d5Num = 1
-d5Date = '2018-03-23'
+d5Num = 3
+d5Date = '2018-04-13'
 d5Cate = '小学'
 
-d6Num = 383
-d6Date = '2018-03-24'
+d6Num = 393
+d6Date = '2018-04-14'
 d6Cate = '与喵共舞'
 
-d7Num = 384
-d7Date = '2018-03-25'
+d7Num = 394
+d7Date = '2018-04-15'
 d7Cate = '与喵共舞'
 
 
